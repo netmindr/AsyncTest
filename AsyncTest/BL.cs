4bb6e633d099db1603d4184ee00b8901b04613ea
David Fisher <david.fisher@navihealth.com>
2019-05-10 00:18:08 -0500

    Initial commit

---
+++ AsyncTest/BL.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DF.Test.AsyncTest
+{
+    public class BL
+    {
+        private string _alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private int _alphaCounter = 1;
+        private int _counter = 1;
+
+        public async Task<string> DoSomethingsNoAwaitAsync()
+        {
+            string response = String.Empty;
+
+            while (_counter <= 10)
+            {
+                // Awaited. So everything stops until this call returns
+                await ReturnInput(_counter);
+
+                // Since this line is not awaited, execution continues in the while block and the work inside NextLetter continues in the background.
+                NextLetter(_counter);
+
+                _counter++;
+            }
+
+            return response;
+        }
+
+        //public async Task<int> DoSomethingAsync()
+        //{
+        //    return await ReturnInput(2);
+        //}
+
+        private async Task ReturnInput(int input)
+        {
+            await Task.Delay(1000);
+            Console.Write(input.ToString());
+        }
+
+        private async Task NextLetter(int index)
+        {
+            string response = _alpha[index].ToString();
+            await Task.Delay(5000);
+            Console.Write(response);
+        }
+    }
+}
---
+++ AsyncTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using DF.Test.AsyncTest;
+
+namespace AsyncTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BL bl = new BL();
+            Console.WriteLine("Hello World!");
+            bl.DoSomethingsNoAwaitAsync().Wait();
+            Console.WriteLine();
+            Console.WriteLine("And we're back!");
+            Console.ReadLine();
+        }
+    }
+}
